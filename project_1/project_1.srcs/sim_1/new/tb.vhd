6df92de3eb25b54b38ef60e2c5e42706d41f0821
PauJordan <62157947+PauJordan@users.noreply.github.com>
2021-11-25 20:14:01 +0100

    Add files via upload

--- project_1/project_1.srcs/sim_1/new/tb.vhd
+++ project_1/project_1.srcs/sim_1/new/tb.vhd
@@ -11,68 +11,74 @@ end tb_o_generic_sr;
 
 architecture tb of tb_o_generic_sr is
 
-    component o_generic_sr
-        generic (
-    Q_width : natural := 12);
-    
-        port (D   : in std_logic;
-              clk,CE,OE : in std_logic;
-              Q   : out std_logic_vector (q_width - 1 downto 0));
-    end component;
+component ads_driver
+       Port (
+    clk: in std_logic; -- 108 MHz STILL NEEDS TO BE MODIFIED 
+    resetn: in std_logic; -- active low
+    sdata1: in std_logic; --because it says: two independent 12-bit successive approximation A/D converters
+    ncs: out std_logic;-- shared between the two converters // it is active low
+    sclk: out std_logic; -- In the system developed in this laboratory session this clock will have a frequency of approximately 18 MHz.
+    leds: out std_logic_vector (7 downto 0)); --we only have one digital output, but the conversion is transmitted in serial.
+  	end component;
 
-    signal D   : std_logic;
-    signal clk : std_logic;
-    signal e   : std_logic;
-    signal Q   : std_logic_vector (11 downto 0);
-    signal ce : std_logic;
+    signal sdata1  : std_logic;
+    signal clk, rstn, sclk, ncs : std_logic;
+    signal leds   : std_logic_vector (7 downto 0);
 
     constant TbPeriod : time := 10 ns; -- EDIT Put right period here
     signal TbClock : std_logic := '0';
     signal TbSimEnded : std_logic := '0';
     
-    constant test_data : std_logic_vector(11 downto 0) := std_logic_vector(to_unsigned(2507, 12));
+    constant test_data : std_logic_vector(14 downto 0) := std_logic_vector(to_unsigned(2507, 15));
+    
 begin
 
-    dut : o_generic_sr
-    port map (D   => D,
-              clk => clk,
-              OE   => e,
-              Q   => Q,
-              CE => ce);
+    dut : ads_driver
+    port map(
+    clk => CLK,
+    resetn => rstn,
+    sclk => SCLK,
+    ncs => NCS,
+    leds => leds,
+    sdata1 => sdata1);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
 
     -- EDIT: Check that clk is really your main clock signal
     clk <= TbClock;
+    rst_pro : process
+    begin
+        rstn <='0';
+        wait for 50 ns;
+        rstn <= '1';
+        wait;
+    end process;
 
     stimuli : process
     begin
         -- EDIT Adapt initialization as needed
-        D <= '0';
-        e <= '0';
+        sdata1 <= 'Z';
         -- Reset generation
         -- EDIT: Check that rst is really your reset signal
-        ce <= '0';
-        wait for 10 ns;
-        ce <= '1';
-        wait for 2 ns;
-         D <= test_data(11);
-        for i in 10 downto 0 loop
+        while 1 = 1 loop
+            wait until falling_edge(ncs);
             wait for 10 ns;
-            D <= test_data(i);
+            
+            for i in 14 downto 0 loop
+                wait until rising_edge(sclk);
+                sdata1 <= 'Z';
+                wait for 5 ns;
+                sdata1 <= test_data(i);
+            end loop;
+            wait until falling_edge(sclk);
+            wait for 5 ns;
+            sdata1 <= 'Z';
+            
+            wait for 50 ns;
         end loop;
-        e <= '1';
-        wait for 20 ns;
-        e <= '0';
-        wait for 20 ns;
-        e <= '1';
-        ce <= '0';
-        wait for 20 ns;
-        e <= '0';
-        wait for 20 ns;
-        e <= '1';
         
+     
         TbSimEnded <= '1';
         wait;
     end process;
