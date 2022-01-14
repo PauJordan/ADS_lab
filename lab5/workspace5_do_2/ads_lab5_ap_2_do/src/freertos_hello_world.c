2c60039ee57873112dfc1ebba6af6ba8998bb2ab
pau.jordan.oliveras <p.jordan.oliveras@c5b1.ALU>
2022-01-14 14:19:22 +0100

    freertos

--- lab5/workspace5_do_2/ads_lab5_ap_2_do/src/freertos_hello_world.c
+++ lab5/workspace5_do_2/ads_lab5_ap_2_do/src/freertos_hello_world.c
@@ -1,30 +1,13 @@
-/*
-    Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
-    Copyright (c) 2012 - 2020 Xilinx, Inc. All Rights Reserved.
-	SPDX-License-Identifier: MIT
-
-
-    http://www.FreeRTOS.org
-    http://aws.amazon.com/freertos
-
-
-    1 tab == 4 spaces!
-*/
 
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-#include "queue.h"
-#include "timers.h"
+
 /* Xilinx includes. */
 #include "xil_printf.h"
 #include "xparameters.h"
 #include "xgpio.h"
-
-#define TIMER_ID	1
-#define DELAY_10_SECONDS	10000UL
-#define DELAY_1_SECOND		1000UL
-#define TIMER_CHECK_THRESHOLD	9
+#include "stdint.h"
 
 
 //app.c
@@ -34,31 +17,30 @@
 #define XADC_DEVICE_ID 		XPAR_XADCPS_0_DEVICE_ID
 #define BUTTON_CHANNEL		1 // Input channel
 
-
+//Peripherals
 static XAdcPs XAdcInst;      /* XADC driver instance */
 XAdcPs *XAdcInstPtr = &XAdcInst;
 static XGpio Gpio; /* The Instance of the GPIO Driver */
+int * axilab5 = (int *) XPAR_LAB5_AXI_DO_0_S00_AXI_BASEADDR; //AXI adress
 
+//Tasks
+static void ui_task( void *pvParameters );
+static void monitoring_task( void *pvParameters );
 
-
-/* The Tx and Rx tasks as described at the top of this file. */
-static void prvTxTask( void *pvParameters );
-static void prvRxTask( void *pvParameters );
-static void vTimerCallback( TimerHandle_t pxTimer );
+//Peripheral initialization function declaration
 static int Peripheral_Init(void);
 
-/*-----------------------------------------------------------*/
+//Task handles
+static TaskHandle_t ui_task_handle;
+static TaskHandle_t monitoring_task_handle;
 
-/* The queue used by the Tx and Rx tasks, as described at the top of this
-file. */
-static TaskHandle_t xTxTask;
-static TaskHandle_t xRxTask;
-static QueueHandle_t xQueue = NULL;
-static TimerHandle_t xTimer = NULL;
-char HWstring[15] = "Hello World";
-long RxtaskCntr = 0;
-XGpio GPIOInst;
-int * axilab5 = (int *) XPAR_LAB5_AXI_DO_0_S00_AXI_BASEADDR;
+
+//Global variables
+static unsigned int temperature_raw = 0;
+static uint16_t temperature = 0;
+static uint16_t threshold = 30;
+
+
 int main( void )
 {
 
@@ -66,55 +48,23 @@ int main( void )
 
 	status = Peripheral_Init();
 
-	const TickType_t x10seconds = pdMS_TO_TICKS( DELAY_10_SECONDS );
+	xil_printf( "Initialized peripherals, status = %d\r\n", status);
 
-	xil_printf( "Hello from Freertos example main, status = %d\r\n", status);
 
-	/* Create the two tasks.  The Tx task is given a lower priority than the
-	Rx task, so the Rx task will leave the Blocked state and pre-empt the Tx
-	task as soon as the Tx task places an item in the queue. */
-	xTaskCreate( 	prvTxTask, 					/* The function that implements the task. */
-					( const char * ) "Tx", 		/* Text name for the task, provided to assist debugging only. */
+	xTaskCreate( 	ui_task, 					/* The function that implements the task. */
+					( const char * ) "UI", 		/* Text name for the task, provided to assist debugging only. */
 					configMINIMAL_STACK_SIZE, 	/* The stack allocated to the task. */
 					NULL, 						/* The task parameter is not used, so set to NULL. */
-					tskIDLE_PRIORITY,			/* The task runs at the idle priority. */
-					&xTxTask );
+					tskIDLE_PRIORITY + 1,			/* The task runs at the idle priority. */
+					&ui_task_handle );
 
-	xTaskCreate( prvRxTask,
-				 ( const char * ) "GB",
+	xTaskCreate( monitoring_task,
+				 ( const char * ) "MON",
 				 configMINIMAL_STACK_SIZE,
 				 NULL,
-				 tskIDLE_PRIORITY + 1,
-				 &xRxTask );
+				 tskIDLE_PRIORITY + 2,
+				 &monitoring_task_handle );
 
-	/* Create the queue used by the tasks.  The Rx task has a higher priority
-	than the Tx task, so will preempt the Tx task and remove values from the
-	queue as soon as the Tx task writes to the queue - therefore the queue can
-	never have more than one item in it. */
-	xQueue = xQueueCreate( 	1,						/* There is only one space in the queue. */
-							sizeof( HWstring ) );	/* Each space in the queue is large enough to hold a uint32_t. */
-
-	/* Check the queue was created. */
-	configASSERT( xQueue );
-
-	/* Create a timer with a timer expiry of 10 seconds. The timer would expire
-	 after 10 seconds and the timer call back would get called. In the timer call back
-	 checks are done to ensure that the tasks have been running properly till then.
-	 The tasks are deleted in the timer call back and a message is printed to convey that
-	 the example has run successfully.
-	 The timer expiry is set to 10 seconds and the timer set to not auto reload. */
-	xTimer = xTimerCreate( (const char *) "Timer",
-							x10seconds,
-							pdFALSE,
-							(void *) TIMER_ID,
-							vTimerCallback);
-	/* Check the timer was created. */
-	configASSERT( xTimer );
-
-	/* start the timer with a block time of 0 ticks. This means as soon
-	   as the schedule starts the timer will start running and will expire after
-	   10 seconds */
-	xTimerStart( xTimer, 0 );
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -129,84 +79,57 @@ int main( void )
 
 
 /*-----------------------------------------------------------*/
-static void prvTxTask( void *pvParameters )
+static void ui_task( void *pvParameters )
 {
-const TickType_t x1second = pdMS_TO_TICKS( DELAY_1_SECOND )/5;
 
 	for( ;; )
 	{
-		/* Delay for 1 second. */
-		vTaskDelay( x1second );
+		vTaskDelay( 40 ); // ~ 25 Hz for debounce
+		//Read buttons
+		uint32_t data = XGpio_DiscreteRead(&Gpio, 1);
 
-		/* Send the next value on the queue.  The queue should always be
-		empty at this point so a block time of 0 is used. */
-		xQueueSend( xQueue,			/* The queue being written to. */
-					HWstring, /* The address of the data being sent. */
-					0UL );			/* The block time. */
+		uint8_t btnr = !!(data & 0x01);
+		uint8_t btnl = !!(data & 0x10);
+
+		if(btnr){ //If button was pressed
+			if(threshold < 80){
+				threshold++; //if below upper limit.
+			}
+		}
+		else if(btnl){
+			if(threshold > 0){
+				threshold--;
+			}
+		}
+
+
 	}
 }
-unsigned int temperature_raw;
-unsigned int temperature;
 
-/*-----------------------------------------------------------*/
-static void prvRxTask( void *pvParameters )
+
+static void monitoring_task( void *pvParameters )
 {
-char Recdstring[15] = "";
-
 	for( ;; )
 	{
-		/* Block to wait for data arriving on the queue. */
-		xQueueReceive( 	xQueue,				/* The queue being read. */
-						Recdstring,	/* Data is read into this address. */
-						portMAX_DELAY );	/* Wait without a timeout for data. */
-		int data = XGpio_DiscreteRead(&GPIOInst, 1);
+		vTaskDelay( 1 ); //1000 Hz critical task.
 
 	    temperature_raw = XAdcPs_GetAdcData(XAdcInstPtr, XADCPS_CH_TEMP);
 	    temperature = (int) XAdcPs_RawToTemperature(temperature_raw);
 
+	    *axilab5 = threshold + ((temperature) << 12);
 
-		if(data){
-			*axilab5 = 0xFFFF;
+		if(temperature < threshold){
+			*axilab5 = *axilab5 & ~(0x01000000);
 		}
-		else{
-			*axilab5 = 0;
+		else {
+			*axilab5 = *axilab5 | 0x01000000 ;
 		}
-
-		/* Print the received data. */
-		xil_printf( "Rx task received string from Tx task: %d\r\n", data);
-		RxtaskCntr++;
 	}
-}
-
-/*-----------------------------------------------------------*/
-static void vTimerCallback( TimerHandle_t pxTimer )
-{
-	long lTimerId;
-	configASSERT( pxTimer );
-
-	lTimerId = ( long ) pvTimerGetTimerID( pxTimer );
-
-	if (lTimerId != TIMER_ID) {
-		xil_printf("FreeRTOS Hello World Example FAILED");
-	}
-
-	/* If the RxtaskCntr is updated every time the Rx task is called. The
-	 Rx task is called every time the Tx task sends a message. The Tx task
-	 sends a message every 1 second.
-	 The timer expires after 10 seconds. We expect the RxtaskCntr to at least
-	 have a value of 9 (TIMER_CHECK_THRESHOLD) when the timer expires. */
-	if (RxtaskCntr >= TIMER_CHECK_THRESHOLD) {
-		xil_printf("Successfully ran FreeRTOS Hello World Example");
-	} else {
-		xil_printf("FreeRTOS Hello World Example FAILED");
-	}
-
-//	vTaskDelete( xRxTask );
-//	vTaskDelete( xTxTask );
 }
 
 
 
+//Peripherail init function definition
 
 int Peripheral_Init()
 {
